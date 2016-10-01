b890d21173d6f315ce9ddab4f291d63b3a16de91
Ziyan <jaraidaniel@gmail.com>
2016-10-01 22:00:57 +0200

    mako: correct product model
    Change-Id: Id070ebc9b17a8fd7a39971fcaa456c91b1ec05f9

--- full_mako.mk
+++ full_mako.mk
@@ -31,7 +31,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 PRODUCT_NAME := full_mako
 PRODUCT_DEVICE := mako
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Mako
+PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
 # Inherit from hardware-specific part of the product configuration
