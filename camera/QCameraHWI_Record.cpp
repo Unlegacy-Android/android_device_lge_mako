78581282ea3b0a01b10f75c0d95d84138020554f
Ziyan <jaraidaniel@gmail.com>
2017-04-02 10:30:44 +0200

    camera: use cached native handles for every video frame
    Otherwise, the framework assign a new handle for each frame, resulting in
    stuttering and fd leakage.
    
    The idea is based on:
    https://source.codeaurora.org/external/gigabyte/platform/hardware/qcom/camera/commit/QCamera2/HAL?h=caf/camera.lnx.1.0.r9-rel&id=3fa680fd3b42cfffe3e41a75204ab56099a91c6f
    
    Change-Id: I543eeedb998e052f3340fb9f2325c568b32c39e8

--- camera/QCameraHWI_Record.cpp
+++ camera/QCameraHWI_Record.cpp
@@ -73,6 +73,8 @@ QCameraStream_record::QCameraStream_record(int cameraId,
   char value[PROPERTY_VALUE_MAX];
   ALOGV("%s: BEGIN", __func__);
 
+  memset(mNativeHandle, 0, sizeof(mNativeHandle));
+
   property_get("persist.debug.sf.showfps", value, "0");
   mDebugFps = atoi(value);
 
@@ -218,7 +220,12 @@ void QCameraStream_record::releaseEncodeBuffer() {
           (struct encoder_media_buffer_type  *)
 #endif
           mHalCamCtrl->mRecordingMemory.metadata_memory[cnt]->data;
-      native_handle_delete(const_cast<native_handle_t *>(packet->meta_handle));
+
+      native_handle_t *nh = const_cast<native_handle_t *>(packet->meta_handle);
+      if (nh) {
+        native_handle_close(nh);
+        native_handle_delete(nh);
+      }
       mHalCamCtrl->mRecordingMemory.metadata_memory[cnt]->release(
         mHalCamCtrl->mRecordingMemory.metadata_memory[cnt]);
 
@@ -346,6 +353,16 @@ status_t QCameraStream_record::processRecordFrame(void *data)
   ALOGV("Send Video frame to services/encoder TimeStamp : %lld",timeStamp);
   mRecordedFrames[frame->video.video.idx] = *frame;
 
+#ifdef USE_NATIVE_HANDLE_SOURCE
+  if (mHalCamCtrl->mStoreMetaDataInFrame) {
+    struct encoder_nativehandle_buffer_type *packet =
+      (struct encoder_nativehandle_buffer_type  *)
+      mHalCamCtrl->mRecordingMemory.metadata_memory[frame->video.video.idx]->data;
+    packet->meta_handle = mNativeHandle[frame->video.video.idx];
+    packet->buffer_type = kMetadataBufferTypeNativeHandleSource;
+  }
+#endif
+
 #ifdef USE_ION
   struct ion_flush_data cache_inv_data;
   int ion_fd;
@@ -472,13 +489,15 @@ status_t QCameraStream_record::initEncodeBuffers()
           (struct encoder_media_buffer_type  *)
 #endif
           mHalCamCtrl->mRecordingMemory.metadata_memory[cnt]->data;
-        packet->meta_handle = native_handle_create(1, 2); //1 fd, 1 offset and 1 size
+        if (!mNativeHandle[cnt])
+          mNativeHandle[cnt] = native_handle_create(1, 2); // 1 fd, 1 offset and 1 size
+        packet->meta_handle = mNativeHandle[cnt];
 #ifdef USE_NATIVE_HANDLE_SOURCE
         packet->buffer_type = kMetadataBufferTypeNativeHandleSource;
 #else
         packet->buffer_type = kMetadataBufferTypeCameraSource;
 #endif
-        native_handle_t * nh = const_cast<native_handle_t *>(packet->meta_handle);
+        native_handle_t *nh = mNativeHandle[cnt];
         nh->data[0] = mHalCamCtrl->mRecordingMemory.fd[cnt];
         nh->data[1] = 0;
         nh->data[2] = mHalCamCtrl->mRecordingMemory.size;
@@ -549,6 +568,14 @@ void QCameraStream_record::releaseRecordingFrame(const void *opaque)
             /* found the match */
             if(MM_CAMERA_OK != cam_evt_buf_done(mCameraId, &mRecordedFrames[cnt]))
                 ALOGE("%s : Buf Done Failed",__func__);
+#ifdef USE_NATIVE_HANDLE_SOURCE
+            struct encoder_nativehandle_buffer_type *packet =
+                (struct encoder_nativehandle_buffer_type  *)
+                mHalCamCtrl->mRecordingMemory.metadata_memory[cnt]->data;
+            native_handle_t *nh = const_cast<native_handle_t *>(packet->meta_handle);
+            native_handle_close(nh);
+            native_handle_delete(nh);
+#endif
             ALOGV("%s : END",__func__);
             return;
         }
