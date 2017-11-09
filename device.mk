8976029139ba71a63761d0657f607a030f8247ff
Artem Borisov <dedsa2002@gmail.com>
2017-11-09 17:45:37 +0300

    Enable binderized gralloc service
    There is no reason to keep using passthrough, as our binder works properly now.
    
    Change-Id: I9b72e9e703cfccc1c9a21f39264412cda0a6e233

--- device.mk
+++ device.mk
@@ -174,6 +174,7 @@ PRODUCT_PACKAGES += \
 	copybit.msm8960 \
 	memtrack.msm8960 \
 	android.hardware.graphics.allocator@2.0-impl \
+	android.hardware.graphics.allocator@2.0-service \
 	android.hardware.graphics.composer@2.1-impl \
 	android.hardware.graphics.mapper@2.0-impl \
 	android.hardware.memtrack@1.0-impl
