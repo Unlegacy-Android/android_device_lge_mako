e5c359ce716e5397175afc4f7cf7572990309208
Daniel Jarai <jaraidaniel@gmail.com>
2018-01-20 20:24:58 +0100

    sensors: replace MultiHAL with a lightweight wrapper
    Wraps our old API 1.0 sensors into API 1.3:
    - fixes sensor flags and maxDelays
    - adds batching support
    - adds flush support (experimental, enabled by default)
    
    Tested by running Android 8.1 CtsSensorTestCases, passing all tests.
    
    Change-Id: I5c365a94de6443772f22a723b1c5095ad385b5be

--- device.mk
+++ device.mk
@@ -244,8 +244,5 @@ PRODUCT_PACKAGES += \
 	android.hardware.sensors@1.0-impl \
 	android.hardware.sensors@1.0-service
 
-PRODUCT_COPY_FILES += \
-	device/lge/mako/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
-
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/widevine/arm-generic/widevine-vendor.mk)
